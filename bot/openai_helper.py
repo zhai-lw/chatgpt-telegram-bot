d7f20c192627077fae2adeeca1ec992c442d0fe5
zhai-lw <zhai_lw@outlook.com>
2025-03-24 15:51:31 +0800

    Update openai_helper.py

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -26,7 +26,7 @@ GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
 GPT_4_VISION_MODELS = ("gpt-4o",)
 GPT_4_128K_MODELS = ("gpt-4-1106-preview", "gpt-4-0125-preview", "gpt-4-turbo-preview", "gpt-4-turbo", "gpt-4-turbo-2024-04-09")
 GPT_4O_MODELS = ("gpt-4o", "gpt-4o-mini", "chatgpt-4o-latest")
-O_MODELS = ("o1", "o1-mini", "o1-preview")
+O_MODELS = ("o1", "o1-mini", "o1-preview", "o3-mini")
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS + GPT_4_VISION_MODELS + GPT_4_128K_MODELS + GPT_4O_MODELS + O_MODELS
 
 def default_max_tokens(model: str) -> int:
